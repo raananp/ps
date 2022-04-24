4a5210813a2dc6af124670d81af72b63b1976f01
Raanan Peretz <47702617+raananp@users.noreply.github.com>
2022-04-24 11:58:31 -0700

    Add files via upload

--- Install-Chrome.ps1
+++ Install-Chrome.ps1
@@ -4,6 +4,7 @@ param(
 [string]$ChromeLink = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B08E9D9DD-49B9-0C89-E561-438EB1C2ACAC%7D%26lang%3Den%26browser%3D5%26usagestats%3D1%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/chrome/install/ChromeStandaloneSetup64.exe'
 )
 $TempFolder = "ChromeStandaloneSetup64"
+$OutFile = "c:\ChromeStandaloneSetup64\ChromeStandaloneSetup64.exe"
 $Chrome = (Get-Item (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe' -ErrorAction SilentlyContinue).'(Default)' -ErrorAction SilentlyContinue).VersionInfo
 function Temp-Folder($Code){
 if ($Code -eq 0){
@@ -14,7 +15,7 @@ if ($Code -eq 0){
     else
         {
             Write-Verbose "Temp folder will be created"
-            New-Item -Path c:\ -Name temp -ItemType directory -Force
+            New-Item -Path c:\ -Name $TempFolder -ItemType directory -Force
         }
 }
 if ($Code -eq 1){
@@ -25,13 +26,14 @@ if ($Code -eq 1){
 Function Install-GoogleChrome{
 try{
             Write-Verbose "Downloading Chrome, This might take a while";
-            Invoke-WebRequest -Uri $ChromeLink -Method Get -OutFile $FilePath -ErrorAction Stop
+            Invoke-WebRequest -Uri $ChromeLink -Method Get -OutFile $OutFile -ErrorAction Stop
            }
         catch{
             $_.Exception; break
              }
-        Write-Verbose "Download Completed" -ForegroundColor Green
-        & $FilePath /silent /install
+        Write-Verbose "Download Completed" 
+        Start-Process -FilePath $OutFile -ArgumentList "/silent", "/install" -Wait
+        #& $OutFile /silent /install
 }
 
 if ($Chrome)
